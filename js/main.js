40232997fac5b567beb981480fbaf84efa5c18c9
MonelosPablo <monelos.p@gmail.com>
2025-09-22 22:04:33 -0300

    Entrega 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 let comprar = true
-let pedidoalfajores=0
-let pedidogalletitas=0
-let pedidogaseosas=0
 let alfajores=0
 let galletitas=0
 let gaseosas=0
-let comprafinal=0
-let valorgalletitas=6
-let valorgaseosas=10
-
-
-
-
 
     let nombre = prompt("Ingresa tu nombre completo")
     let direccion = prompt("Ingresa la dirección donde querés recibir tu compra")
@@ -24,11 +14,12 @@ let valorgaseosas=10
 
 while(comprar) {
 
-    let menu = parseInt(prompt("Ingrese\n 1 para comprar \n 2 para pagar \n 3 para ver el carrito de compras \n otro numero para salir"))
+    let menu = parseInt(prompt("Ingrese\n 1 para comprar \n 2 para pagar \n 3 para ver mis datos \n otro numero para salir"))
 
     switch(menu) {
         case 1:
             console.log("¿Qué te gustaría comprar?")
+
             let carrito = parseInt(prompt("1 para Alfajores\n 2 para Galletitas\n 3 para Gaseosas \n otro número para ir atrás"))
                 switch(carrito) {
                     case 1:
@@ -42,6 +33,7 @@ while(comprar) {
                     case 2:
                         let galletitas = parseInt(prompt("¿Cuantas galletitas agrego?"))
                         function cuentagalletitas () {
+                            
                             const valorgalletitas = 6
                         return galletitas*valorgalletitas
                         }
@@ -62,25 +54,28 @@ while(comprar) {
             break
         case 2:
             console.log("El total por la compra es de $"+totalcompra()+"\nHacé tu pagó por transferencia al siguiente alias: ---------")
-            break
-        case 3:
-            console.log("serian "+cuentaalfajores(alfajores)+" de los alfajores")
+            console.log("siendo "+cuentaalfajores(alfajores)+" de los alfajores")
             console.log(cuentagalletitas(galletitas)+" de las galletitas")
             console.log("y "+cuentagaseosas(gaseosas)+" de las gaseosas")
-            function totalcompra () {
-            let comprafinal = cuentaalfajores(alfajores)+cuentagalletitas(galletitas)+cuentagaseosas(gaseosas)
-            return comprafinal
-            }
+                function totalcompra () {
+                let comprafinal = cuentaalfajores(alfajores)+cuentagalletitas(galletitas)+cuentagaseosas(gaseosas)
+                return comprafinal
+                }
             console.log(totalcompra())
-            console.log("El total es de "+totalcompra())
+            console.log("El total es de "+totalcompra()+"\n¡Muchas gracias!")
+            break
+        case 3:
+            let datos = ["Nombre: "+nombre, "Dirección de la entrega: "+direccion, "Tu Whatsapp: "+telefono];
+            console.log(datos);
             break
         default:
-            console.log("Opcion incorrecta")
+            console.log("Salir")
+            break
     }
 
     let confirmacion = prompt("Desea hacer otra operación? (si/no)").toLowerCase()
-    if(comprar == "no") {
+    if(confirmacion == "no") {
         comprar = false
-        console.log("Retire su tarjeta!")
+        alert("Gracias por comprar en Kiosco Virtual");
     }
 }
